5a20a9ba529752ee2bb83f3df1ddf92077f387eb
abhilashess <abhilash.jaiswal@essindia.com>
2016-02-26 17:17:05 +0530

    Intial Commit

---
+++ poc.sql
@@ -0,0 +1,29 @@
+
+
+
+CREATE TABLE `test` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+
+
+INSERT INTO test VALUES
+("1","TEST"),
+("2","TEST"),
+("3","TEST"),
+("4","TEST"),
+("5","TEST");
+
+
+
+
+CREATE TABLE `test2` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
+
